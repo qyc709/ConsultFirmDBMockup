034cae51edd3f7c1cc507e6709791091240f16ac
qyc709 <00.chen.0709@gmail.com>
2025-04-10 16:36:17 -0700

    test queries

--- src/generate_consulting_firm_data.py
+++ src/generate_consulting_firm_data.py
@@ -343,4 +343,4 @@ def generate_consulting_firm_data(start_year, initial_no_of_months, no_of_update
     
 
 if __name__ == '__main__':
-    generate_consulting_firm_data(2024, 6, 4)
+    generate_consulting_firm_data(2024, 6, 5)
